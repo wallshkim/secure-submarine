88b0f746a3d48248ec9f711b15779a404dd114e8
Heather Walls <wallshkim@gmail.com>
2019-11-13 10:31:05 -0600

    Updates secret router to authorize based on clearance level

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
-router.get('/', (req, res) => {
+
+router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    pool.query('SELECT * FROM "secret" WHERE $1 >= "secret"."secrecy_level";', [req.user.clearance_level])
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
             res.sendStatus(500);
         });
 });
+
+// router.get('/', rejectUnauthenticated, (req, res) => {
+//     res.send(req.user);
+// });
 
 module.exports = router;
